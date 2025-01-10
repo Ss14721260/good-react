c54d26277a692b254860e0c711eaa185c87d2863
Ss14721260 <ssingh.8721382@gmail.com>
2025-01-10 17:56:41 +0530

    tailwind project

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,33 +1,30 @@
-import { useState,useEffect} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState} from 'react'
 import './App.css'
-import { use } from 'react'
-
 function App() {
-  const [Counter, statechangerVariable]  =  useState(0);
-  // let Counter = 0; 
-  const addValue = () => { 
-    statechangerVariable    (Counter+1)
-      
+  const [a,ad1] = useState(0)
+  const [b,ad2] = useState(0)
+  const addValueIna = () => { 
+    ad1(a+1)
     }
-    const removeValue = () => { 
-      if(Counter>0)
-      statechangerVariable(Counter-1)
-        
-      }
-
+  const addValueInb = () => {
+    ad2(b+1)
+  }
+  
 
   return (
-    <>
-      <h1>React with Suraj</h1>
-      <h2>Counter value: {Counter}</h2>
-      <button onClick={addValue}>Add value</button>
+   <>   
+    <h1>React with Suraj</h1>
+  
+    <h2>Counter value:</h2>
+      <button onClick={addValueIna}>Click to Add value in A: {a}</button>
       <br />
-      <button onClick={removeValue}>Remove value: {Counter}</button>
-      <p>{Counter}</p>
-       
-    </>
+      <button onClick={addValueInb}>Click to Add value in B: {b}</button>
+      <h2>Result A+B={a+b}: </h2>
+      <br />
+    
+      </>  
+    
   )
 }
+
 export default App
---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>03TailWindProps Project</h1>
+      <br />
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <br />
+    <h1 className='bg-green-400 text-red-500 p-4 rounded-xl' >Tailwind Test</h1><br /><br /><br /><br /><br />
+
+    </>
+  )
+}
+
+export default App
