5a85c5e86820155b080b8c23148b933f8e7e4183
Ss14721260 <ssingh.8721382@gmail.com>
2025-01-14 17:57:08 +0530

    backgroun changer has use of onChnges= setLength(e.target.value)

--- pwdchanger/src/App.jsx
+++ pwdchanger/src/App.jsx
@@ -18,22 +18,45 @@ const passwordGenrator = useCallback(() => {
 
   return (
     <>
-      <div className="w-full max-w-md max-auto shadow-md 
-        rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-700">
-          <h1 className="text-center text-white">
-          Password Generator</h1>
-          <div className="flex shadow rounded-lg overflow-hidden mb-4">
+      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-800'>
+          <h1 className='text-center text-white'>Password Generator </h1>
+          <div className='flex shadow rounded-lg overflow-hidden mb-4'>
         <input 
           type="text" 
           value={password}
-          className="outline-none w-full py-1 px-3"
+          className='outline-none w-full py-1 px-3'
           placeholder="password"
           readOnly
         />
+        <button className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">
+          Copy
+        </button>
         </div>
-        
+        <div className="flex fap-x-2 text-sm"></div>
+        <div className="flex items-center gap-x-1 ">
+
+          <input type="range" 
+          min={8}
+          max={100}
+          value={length}
+          className="cursor-pointer"
+          onChange={(e)=>{setLength(e.target.value)}}
+          />
+          <label>Length: {length}</label>
+        </div>
+          <div className="flex items-center gap-x-1 ">
+
+           <input type="checkbox" 
+          defaultChecked={numberAllowed}
+          id="numberInput"
+          onChange={() => {
+              setNumberAllowed((prev) => !prev);
+          }}
+        /> <label htmlFor="numberInput">Numbers</label>
+          </div>
+           
+
       </div>
- 
     </>
   )
 }
